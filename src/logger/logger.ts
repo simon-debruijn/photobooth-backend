24e1f9932d88bbf79dd4469e6df819df2d3c441a
Simon de Bruijn <simon.debruijn@publiq.be>
2022-04-03 13:53:56 +0200

    Change logger config for development

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -3,13 +3,17 @@ import pino, { LoggerOptions, StreamEntry } from 'pino';
 const isDev = process.env.NODE_ENV !== 'production';
 
 const streams: StreamEntry[] = [
-  ...(!isDev ? [{ stream: process.stdout }] : []),
-  {
-    stream: pino.destination({
-      dest: process.env.LOG_DIRECTORY || `${__dirname}/../../logs/combined.log`,
-      mkdir: !process.env.LOG_DIRECTORY,
-    }),
-  },
+  ...(isDev ? [{ stream: process.stdout }] : []),
+  ...(!isDev
+    ? [
+        {
+          stream: pino.destination({
+            dest: process.env.LOG_DIRECTORY || `${__dirname}/../../logs/combined.log`,
+            mkdir: !process.env.LOG_DIRECTORY,
+          }),
+        },
+      ]
+    : []),
 ];
 
 const options: LoggerOptions = {
