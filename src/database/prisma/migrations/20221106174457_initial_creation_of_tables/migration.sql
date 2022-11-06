187e4dc3caaff288735d2159c01d1400357936a6
simon-debruijn <simon.debruijn@hotmail.com>
2022-11-06 18:46:36 +0100

    Add initial creation database migration

---
+++ src/database/prisma/migrations/20221106174457_initial_creation_of_tables/migration.sql
@@ -0,0 +1,29 @@
+-- DropTable
+DROP TABLE "user" IF EXISTS;
+DROP TABLE "order" IF EXISTS;
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "tokens" TEXT[],
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "url_friendly_id" TEXT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DECIMAL(10,2) NOT NULL,
+    "images" TEXT[],
+
+    CONSTRAINT "order_pkey1" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "order" ADD CONSTRAINT "order_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
